109c7fe62f56b724d20cb79ebb9ce62ce235c833
Akil Hamilton <akil.james83@gmail.com>
2018-12-24 12:46:03 -0500

    Change to problem 771

--- question771.cpp
+++ question771.cpp
@@ -4,11 +4,7 @@ public:
         map<char, int> jmap;
         int count = 0;
         for (int i = 0; i < J.length(); i++){
-            if (jmap.find(J[i]) != jmap.end()){
-                jmap[J[i]] += 1;
-            } else{
-                jmap.insert(make_pair(J[i], 1));
-            }  
+            jmap.insert(make_pair(J[i], 1));
         }
         for (int i = 0; i < S.length(); i++) {
             if (jmap.find(S[i]) != jmap.end())
