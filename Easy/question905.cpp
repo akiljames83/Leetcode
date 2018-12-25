0754f75d85b0591194bae39f4a0fe6391283f2b7
Akil Hamilton <akil.james83@gmail.com>
2018-12-25 00:37:10 -0500

    Update question905.cpp

--- Easy/question905.cpp
+++ Easy/question905.cpp
@@ -13,3 +13,27 @@ public:
         return end;
     }
 };
+
+// Much Slower Deque Implementation:
+#include <deque>
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& A) {
+        deque<int> end;
+        if(not A.size()) return A;
+        
+        for(auto it = --A.end(); it >= A.begin(); it--){
+            if ((*it)%2 == 0){
+                end.push_front(*it);
+            } else {
+                end.push_back(*it);
+            }
+        }
+        vector<int> ans;
+        for(auto it = end.begin(); it != end.end(); it++){
+            ans.push_back(*it);
+        }
+        return ans;
+        
+    }
+};
