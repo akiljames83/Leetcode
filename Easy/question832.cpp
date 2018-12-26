77ef808f5a61072e04cf1be4359145113be14ce2
Akil Hamilton <akil.james83@gmail.com>
2018-12-25 22:26:09 -0500

    Update question832.cpp

--- Easy/question832.cpp
+++ Easy/question832.cpp
@@ -21,3 +21,23 @@ public:
         return A;
     }
 };
+
+// Faster solution, inplace, O(n*m), using easy bit manip;
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) {
+        if (A.size() == 0 || A[0].size() == 0) return A;
+        
+        for (int i = 0; i < A.size(); i++){
+            int begin{};
+            int end{A[0].size() -1 };
+            while (end - begin > 0) {
+                int temp = A[i][begin];
+                A[i][begin++] = A[i][end] ^ 0x01;
+                A[i][end--] = temp ^ 0x01;
+            }
+            if (end == begin) A[i][begin] ^= 0x01;
+        }
+        return A;
+    }
+};
