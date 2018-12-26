2ad68ebbd83fe95b4331c5b3c308e4e7378a90dd
Akil Hamilton <akil.james83@gmail.com>
2018-12-25 22:19:27 -0500

    Add question832.cpp

---
+++ Easy/question832.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> flipAndInvertImage(vector<vector<int>>& A) {
+        if (A.size() == 0 || A[0].size() == 0) return A;
+        // Flip the image
+        for (int i = 0; i < A.size(); i++){
+            int begin{};
+            int end{A[0].size() -1 };
+            while (end - begin > 0) {
+                int temp = A[i][begin];
+                A[i][begin++] = A[i][end];
+                A[i][end--] = temp;
+            }
+        }
+        // invert the image
+        for (int i = 0; i < A.size(); i++){
+            for (int j = 0; j < A[0].size(); j++){
+                A[i][j] ^= 0x01;
+            }
+        }
+        return A;
+    }
+};
