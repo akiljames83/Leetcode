e046228db8ec49cf82a9b20d61659e85e5e087fd
Akil Hamilton <akil.james83@gmail.com>
2018-12-27 00:28:37 -0500

    Update question852.cpp

--- Easy/question852.cpp
+++ Easy/question852.cpp
@@ -14,3 +14,15 @@ public:
         return binarySearch(A, 0, A.size() - 1);
     }
 };
+
+// Old version O(n) runtime
+class Solution {
+public:
+    int peakIndexInMountainArray(vector<int>& A) { 
+        int result{};
+        for(int i = 1; i < A.size() - 1; i++){
+            if (A[i] > A[i-1] && A[i] > A[i + 1]) result = i;
+        }
+        return result;
+    }
+};
