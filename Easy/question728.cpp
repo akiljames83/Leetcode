b999b9ec6aaf2e7fd978567f0eae9dc53fd9397e
Akil Hamilton <akil.james83@gmail.com>
2018-12-27 01:50:00 -0500

    Update question728.cpp

--- Easy/question728.cpp
+++ Easy/question728.cpp
@@ -18,16 +18,28 @@ public:
         } 
         return true;
     }
-    /*
-        int original = num;
-        int digit;
-        while(num > 0){
-            digit = num % 10;
-            if(digit == 0 || original % digit != 0)
-                return false;
-            num /= 10;
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> ans;
+        for(int i = left; i <= right; i++){
+            if (check(i)) ans.push_back(i);
         }
-    */
+        return ans;
+    }
+};
+
+//  Faster Solution: Same logic but implemented properly
+class Solution {
+public:
+    bool check(int original){
+        int num = original;
+        while( num != 0) {
+            int temp{num%10};
+            if ((temp == 0) || (original % temp)) return false;
+            num /= 10;
+        } 
+        return true;
+    }
+
     vector<int> selfDividingNumbers(int left, int right) {
         vector<int> ans;
         for(int i = left; i <= right; i++){
