7e64f636ebfe6253875b916e6ee5ca0ff8ac87ab
Akil Hamilton <akil.james83@gmail.com>
2018-12-27 10:02:15 -0500

    Update question561.cpp

--- Easy/question561.cpp
+++ Easy/question561.cpp
@@ -9,3 +9,45 @@ public:
         return sum;
     }
 };
+
+// Map version -> 36ms
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        vector<int> t(20001);
+        for(int& i : nums)
+            t[i + 10000]++;
+        int ans = 0;
+        bool evenNum = true;
+        for(int j = 0; j < t.size(); ++j){
+            while(t[j]){
+                if(evenNum)
+                    ans+=(j-10000);
+                t[j]--;
+                evenNum = !evenNum;
+            }
+        }
+        return ans;
+    }
+};
+
+// O(n) version -> 24ms; based on the paramters of the question -> since the numbers can range from -10000 to 10000 inclusive.
+class Solution {
+public:
+    int arrayPairSum(vector<int>& nums) {
+        vector<int> t(20001);
+        for(int& i : nums)
+            t[i + 10000]++;
+        int ans = 0;
+        bool evenNum = true;
+        for(int j = 0; j < t.size(); ++j){
+            while(t[j]){
+                if(evenNum)
+                    ans+=(j-10000);
+                t[j]--;
+                evenNum = !evenNum;
+            }
+        }
+        return ans;
+    }
+};
