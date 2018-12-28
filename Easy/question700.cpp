fa1dc8a2842cdbbe1a21214bbc8813b392195df0
Akil Hamilton <akil.james83@gmail.com>
2018-12-28 07:31:59 -0500

    Update question700.cpp

--- Easy/question700.cpp
+++ Easy/question700.cpp
@@ -23,3 +23,26 @@ public:
         return search(root, val);
     }
 };
+
+// Better Solution - Realizing it is a binary search tree and not just a binary tree
+class Solution {
+public:
+     TreeNode* search(TreeNode* root, int val){
+        if (root == NULL) 
+            return nullptr;
+        if (root->val == val)
+            return root;
+        if (root->left == NULL and root->right == NULL) 
+            return nullptr;
+        if (root->val > val) 
+            return search(root->left, val);
+        
+        return search(root->right, val);
+            
+    }
+    TreeNode* searchBST(TreeNode* root, int val) {
+        if (root == NULL) 
+            return root;
+        return search(root, val);
+    }
+};
