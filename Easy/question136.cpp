562c1492278382d63e5a0d24296c30186e39d10a
Akil Hamilton <akil.james83@gmail.com>
2019-01-03 05:00:28 -0500

    Update question136.cpp

--- Easy/question136.cpp
+++ Easy/question136.cpp
@@ -11,3 +11,16 @@ public:
 };
 
 // Set:
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_set<int> set;
+        for (int i = 0; i < nums.size(); i++){
+        	int cur = nums[i];
+            if (set.find(cur) != set.end())
+            	set.erase(cur);
+            else set.insert(cur);
+        }
+        return *set.begin();
+    }
+};
