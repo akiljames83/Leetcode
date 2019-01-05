ed83e5f43d07ccdb38fae5e6d5d03fd7fd75cfe4
Akil Hamilton <akil.james83@gmail.com>
2019-01-05 18:59:08 -0500

    Update question888.cpp

--- Easy/question888.cpp
+++ Easy/question888.cpp
@@ -17,3 +17,22 @@ public:
         }
     }
 };
+
+// Better solution using a set:
+class Solution {
+public:
+    vector<int> fairCandySwap(vector<int>& A, vector<int>& B) {
+        int sa{}, sb{};
+        for (auto a : A)
+            sa += a;
+        for (auto b : B)
+            sb += b;
+        unordered_set<int> set{B.begin(), B.end()};
+        int dif = (sb - sa)/2;
+        for (int i = 0; i < A.size(); i++){
+            if (set.find(A[i] + dif) != set.end())
+                return {A[i], A[i]+dif};
+        }
+        
+    }
+};
