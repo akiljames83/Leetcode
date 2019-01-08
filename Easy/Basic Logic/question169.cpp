eece7fd48a3035b1e99ffeeb534e6309926cd303
Akil Hamilton <akil.james83@gmail.com>
2019-01-08 10:13:54 -0500

    Add question169.cpp

---
+++ Easy/Basic Logic/question169.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        if (nums.size() == 1) return nums.front();
+        unordered_map<int, int>map;
+        int size = nums.size()/2;
+        for (auto i : nums){
+            if (map.find(i) != map.end()){
+                if (map[i]++ >= size)
+                    return i;
+            } else
+                map[i] = 1;
+        }
+        
+    }
+};
