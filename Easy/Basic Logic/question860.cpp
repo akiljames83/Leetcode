2b3ac978955ed4512a818c633749d9c35405c9ea
Akil Hamilton <akil.james83@gmail.com>
2019-01-09 13:52:45 -0500

    Update question860.cpp

--- Easy/Basic Logic/question860.cpp
+++ Easy/Basic Logic/question860.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     bool lemonadeChange(vector<int>& bills) {
-        int fives{0};
-        int tens{0};
-        for (int i = 0; i < bills.size(); i++){
+        int fives{0}, tens{0};
+        for (int i = 0; i < bills.size(); i++) {
             if (bills[i] == 5) 
                 fives++;
             else if (bills[i] == 10) {
-                if (fives){
-                    fives--;
+                if (fives--)
                     tens++;
-                }
                 else return false;
             } else if (bills[i] == 20){
                 if (fives and tens) {
