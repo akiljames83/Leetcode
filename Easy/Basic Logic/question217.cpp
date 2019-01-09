4b0bf169eab591661389f0c1ec442b442e337cd0
Akil Hamilton <akil.james83@gmail.com>
2019-01-09 16:38:46 -0500

    Update question217.cpp

--- Easy/Basic Logic/question217.cpp
+++ Easy/Basic Logic/question217.cpp
@@ -11,3 +11,18 @@ public:
         return false;
     }
 };
+
+// Clean and Fast Solution:
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        // Sort the vector. Need for std::unique to work.
+        sort(nums.begin(), nums.end());
+        
+        // Collect all the unique values to the front.
+        auto it = unique(nums.begin(), nums.end());
+        
+        // Return result.
+        return ((it == nums.end()) ? false : true);
+    }
+};
