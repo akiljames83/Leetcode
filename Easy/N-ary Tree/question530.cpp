c40dbf26fecb321f5a2d2cd16ee4683e427636be
Akil Hamilton <akil.james83@gmail.com>
2019-01-10 16:52:15 -0500

    Add question530.cpp

---
+++ Easy/N-ary Tree/question530.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void pot(TreeNode * root, int & min ,int & prev) {
+        if (!root)
+            return;
+        
+        if (root->left)
+            pot(root->left, min, prev);
+        
+        if (prev > -1)
+            if (root->val - prev < min)
+                min = root->val - prev;
+        
+        prev = root->val;
+        
+        if (root->right)
+            pot(root->right, min, prev);
+    }
+    int getMinimumDifference(TreeNode* root) {
+        int min{INT_MAX};
+        int prev{-1};
+        pot(root, min, prev);
+        return min;
+    }
+};
