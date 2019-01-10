c5f0867d04d03ba24bdac0aebc077e5345635bc8
Akil Hamilton <akil.james83@gmail.com>
2019-01-10 16:27:34 -0500

    Add question538.cpp

---
+++ Easy/N-ary Tree/question538.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void pot(TreeNode* node, int & total){
+        if (!node)
+            return;
+        if (node->right)
+            pot(node->right, total);
+        
+        total += node->val;
+        
+        if (node->left)
+            pot(node->left, total);
+            
+    }
+    void iot(TreeNode* node, int & total){
+        if (!node)
+            return;
+        
+        if (node->left)
+            iot(node->left, total);
+            
+        total -= node->val;
+        node->val += total;
+        
+        if (node->right)
+            iot(node->right, total);
+    }
+    TreeNode* convertBST(TreeNode* root) {
+        int total{0};
+        pot(root, total);
+        iot(root, total);
+        return root;
+    }
+};
