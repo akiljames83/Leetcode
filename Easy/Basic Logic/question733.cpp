195d28f751462704103269ec180bc3348fcfc42b
Akil Hamilton <akil.james83@gmail.com>
2019-01-10 22:42:58 -0500

    Update question733.cpp

--- Easy/Basic Logic/question733.cpp
+++ Easy/Basic Logic/question733.cpp
@@ -6,58 +6,41 @@ public:
         return false;
     }
     void addPoints(queue<pair<int, int>>& q, pair<int,int>pos, pair<int, int> dims, unordered_set<int> &set){
-        //cout << "Trying: (" << pos.first + 1 << "," << pos.second << ")" << endl;
         if (validPoint(pos.first + 1, pos.second, dims)) {
-            //cout << "GOOD.1" << endl;
             pair<int, int> tmp = make_pair(pos.first + 1, pos.second);
             int t_val = dims.second*tmp.first + tmp.second;
             
             if (set.find(t_val) == set.end()) {
-                //cout << t_val << endl;
-                //cout << "(" << pos.first + 1<< "," << pos.second<< ")" << endl;
                 q.push(make_pair(pos.first + 1, pos.second));
                 set.insert(t_val);
             }
                 
         }
         
-        //cout << "Trying: (" << pos.first << "," << pos.second + 1 << ")" << endl;
         if (validPoint(pos.first , pos.second + 1, dims)) {
             pair<int, int> tmp = make_pair(pos.first , pos.second + 1);
             int t_val = dims.second*tmp.first + tmp.second;
-            //cout << "C2 " <<  t_val << endl;
             if (set.find(t_val) == set.end()) {
-                //cout << t_val << endl;
-                //cout << "(" << pos.first - 1<< "," << pos.second<< ")" << endl;
-                //cout << "Very Good" << endl;
                 q.push(make_pair(pos.first , pos.second + 1));
                 set.insert(t_val);
             }
                 
         }
         
-        //cout << "Trying: (" << pos.first - 1 << "," << pos.second << ")" << endl;
         if (validPoint(pos.first - 1, pos.second, dims)) {
-            //cout << "GOOD.3" << endl;
             pair<int, int> tmp = make_pair(pos.first - 1, pos.second);
             int t_val = dims.second*tmp.first + tmp.second;
             if (set.find(t_val) == set.end()) {
-                //cout << t_val << endl;
-                //cout << "(" << pos.first - 1<< "," << pos.second<< ")" << endl;
                 q.push(make_pair(pos.first - 1, pos.second));
                 set.insert(t_val);
             }
                 
         }
         
-        //cout << "Trying: (" << pos.first << "," << pos.second - 1 << ")" << endl;
         if (validPoint(pos.first , pos.second - 1, dims)) {
-            //cout << "GOOD.4" << endl;
             pair<int, int> tmp = make_pair(pos.first , pos.second - 1);
             int t_val = dims.second*tmp.first + tmp.second;
             if (set.find(t_val) == set.end()) {
-                //cout << t_val << endl;
-                //cout << "(" << pos.first + 0<< "," << pos.second - 1<< ")" << endl;
                 q.push(make_pair(pos.first , pos.second - 1));
                 set.insert(t_val);
             }
@@ -74,11 +57,8 @@ public:
         unordered_set<int> set;
         set.insert(sr*dims.second + sc);
         addPoints(points, make_pair(sr, sc), dims, set);
-        //cout << "DESIRED COLOR: " << curColor << endl;
         while (!points.empty()){
             auto tmp = points.front();
-            //cout << "("<< tmp.first << ", " << tmp.second << ")";
-            //cout << " Color -> " << image[tmp.first][tmp.second] << endl;
             if (image[tmp.first][tmp.second] == curColor){
                 addPoints(points, tmp, dims, set);
                 image[tmp.first][tmp.second] = newColor;
