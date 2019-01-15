cef621658468f9775a304c27411a2a3490ec84dc
Akil Hamilton <akil.james83@gmail.com>
2019-01-15 06:32:46 -0500

    Add question409.cpp

---
+++ Easy/StringManip/question409.cpp
@@ -0,0 +1,28 @@
+#define isUp(c) ((c >= 'A') and (c <= 'Z'))
+#define isLow(c) ((c >= 'a') and (c <= 'z'))
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        vector<int> map(52,0);
+        for (auto c : s) {
+            if (isLow(c))
+                map[c-'a']++;
+            else
+                map[c-'A'+26]++;
+        }
+        bool single{false};
+        int count{}, max{};
+        for (auto i : map){
+            if (!(i%2))
+                count += i;
+            else {
+                if (!single)
+                    single = true;
+                count += i - 1;
+            }
+        }
+        if (single)
+            count++;
+        return count;
+    }
+};
