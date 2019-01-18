a5ab45f316b5483bd46e51fa87929fbf631b6b5c
Akil Hamilton <akil.james83@gmail.com>
2019-01-18 14:08:33 -0500

    Add question80.cpp

---
+++ Medium/Basic Logic/question80.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        if (!nums.size())
+            return 0;
+        int prev{nums[0]};
+        int count_dub{0};
+        int len = nums.size() - 1;
+        int rlen = len + 1;
+        for (int i = 1; i < nums.size(); i++){
+            if (nums[i] == prev) {
+                if (++count_dub >= 2) {
+                    nums[i] = INT_MIN;
+                    rlen--;
+                }
+            } else {
+                count_dub = 0;
+                prev = nums[i];
+            }
+        }
+        int back = len;
+        for (int i = 0; i < len; i++) {
+            if (nums[i] == INT_MIN) {
+                nums[i] = nums[back];
+                nums[back--] = INT_MAX;
+            } 
+            while(nums[i] == INT_MIN) {
+                nums[i] = nums[back];
+                nums[back--] = INT_MAX;
+                if (i == rlen)
+                    break;
+            }
+        }
+        std::sort(nums.begin(), nums.begin()+rlen);
+        return rlen;
+    }
+};
