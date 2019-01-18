f095ee229fd3079f0a9e6b9dfe518734eb490b75
Akil Hamilton <akil.james83@gmail.com>
2019-01-18 14:16:32 -0500

    Update question80.cpp

--- Medium/Basic Logic/question80.cpp
+++ Medium/Basic Logic/question80.cpp
@@ -35,3 +35,17 @@ public:
         return rlen;
     }
 };
+
+// MUCH Cleaner solution:
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int index = 0;
+        for(int i=0; i<nums.size(); i++)
+        {
+            if(index<2 || nums[index-2] != nums[i])
+                swap(nums[index++], nums[i]);
+        }
+        return index;
+    }
+};
