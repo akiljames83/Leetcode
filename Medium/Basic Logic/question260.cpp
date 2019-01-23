92cedce169cb8ead082fa01d891806585065cd14
Akil Hamilton <akil.james83@gmail.com>
2019-01-23 12:33:04 -0500

    Update question260.cpp

--- Medium/Basic Logic/question260.cpp
+++ Medium/Basic Logic/question260.cpp
@@ -13,3 +13,29 @@ public:
         return {*set.begin(), *++(set.begin())};
     }
 };
+
+// BEAUTIFUL SOLUTION: Explanation from: https://leetcode.com/terrancepyrope/
+class Solution {
+public:
+    vector<int> singleNumber(vector<int>& nums) {
+        int aXorB = 0;
+        for (int num : nums) {
+            aXorB ^= num;
+        }
+        int differingBit = 1;
+        while ((differingBit & aXorB) == 0) {
+            differingBit <<= 1;
+        }
+        
+        int a = 0;
+        int b = 0;
+        for (int num : nums) {
+            if ((num & differingBit) == 0) {
+                a ^= num;
+            } else {
+                b ^= num;
+            }
+        }
+        return {a, b};
+    }
+};
