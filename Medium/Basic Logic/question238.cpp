f9c9d6b2f87fae0ba65f387993df88e7b3b1ed78
Akil Hamilton <akil.james83@gmail.com>
2019-02-12 07:30:04 -0500

    Update question238.cpp

--- Medium/Basic Logic/question238.cpp
+++ Medium/Basic Logic/question238.cpp
@@ -1,3 +1,28 @@
+// O(N) Time; O(1) Space:
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int>res = nums;
+
+        // Go to right
+        for (int i = 1; i < nums.size(); i++)
+            res[i] *= res[i-1];
+        
+        // tracker for the left direction
+        int t = 1;
+        for (int i = nums.size() - 1; i > 0; i--) {
+            // essentially multiplying left side and right side
+            res[i] = res[i-1]*t;
+            t *= nums[i];
+        }
+        res[0] = t;
+        
+        return res;
+            
+    }
+};
+
+// O(N) Time and Space:
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
