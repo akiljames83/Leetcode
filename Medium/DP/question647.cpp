6e10c91daffe3a69204f569a0d122891642844ff
Akil Hamilton <akil.james83@gmail.com>
2019-02-12 08:46:49 -0500

    Update question647.cpp

--- Medium/DP/question647.cpp
+++ Medium/DP/question647.cpp
@@ -21,3 +21,23 @@ public:
         return count;
     }
 };
+
+// Build up approach:
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int count{s.size()};
+        // build up approach
+        for (int i = 0; i != s.size(); i++) {
+            int l = i;
+            int r = i + 1;
+            while (l >= 0 and r != s.size() and s[l--] == s[r++])
+                count++;
+            l = i - 1;
+            r = i + 1;
+            while (l >= 0 and r != s.size() and s[l--] == s[r++])
+                count++;
+        }
+        return count;
+    }
+};
