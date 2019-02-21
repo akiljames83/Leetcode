1b45bc4921890c4c471d52ab3db818b3d4322fe0
Akil Hamilton <akil.james83@gmail.com>
2019-02-20 19:20:31 -0500

    Update question908.cpp

--- Easy/Basic Logic/question908.cpp
+++ Easy/Basic Logic/question908.cpp
@@ -1,3 +1,20 @@
+// Best Solution:
+class Solution {
+public:
+    int smallestRangeI(vector<int>& A, int K) {
+        int min{INT_MAX}, max{INT_MIN};
+        for (auto c : A)
+        {
+            if (c > max) max = c;
+            if (c < min) min = c;
+        }
+        int dif = max - min;
+        if (dif <= K*2)
+            return 0;
+        return dif - 2*K;
+    }
+};
+
 class Solution {
 public:
     int smallestRangeI(vector<int>& A, int K) {
