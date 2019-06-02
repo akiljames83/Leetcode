41ff4b52adabd5dac479839955872ae78b06fbd4
Akil Hamilton <akil.james83@gmail.com>
2019-06-02 08:49:03 -0400

    Add question807.cpp

---
+++ Medium/Basic Logic/question807.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
+        int count{};
+        vector<vector<int>> n_grid = grid;
+        vector<int> max_row(grid[0].size(), 0);
+        
+        for (int i = 0; i < grid.size(); i++) {
+            int max = *max_element(grid[i].begin(), grid[i].end());
+            
+            for (int j = 0; j < grid[i].size(); j++) {
+                if (grid[i][j] > max_row[j])
+                    max_row[j] = grid[i][j];
+                n_grid[i][j] = max; 
+            }
+        }
+        
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[i].size(); j++) {
+                count += (min(max_row[j], n_grid[i][j]) - grid[i][j]);
+            }
+        }        
+        
+        return count;
+    }
+};
